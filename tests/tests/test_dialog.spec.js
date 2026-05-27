ac1a43bac27ce349d8443aa30ec89377601c1e0b
khoa.pham1 <khoap1862@gmail.com>
2026-05-27 15:03:47 +0700

    register v1

--- tests/tests/test_dialog.spec.js
+++
@@ -1,9 +0,0 @@
-import { test } from '@playwright/test';
-
-test('Dump new HTML', async ({ page }) => {
-  await page.goto('https://banve.my-board.org/auth/register.php');
-  const bodyHtml = await page.content();
-  console.log('--- HTML DUMP START ---');
-  console.log(bodyHtml);
-  console.log('--- HTML DUMP END ---');
-});
